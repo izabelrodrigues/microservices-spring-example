27446d1c58f885a5c690cbe09a47c93022296986
Izabel Rodrigues <izabel.castro.rodrigues@gmail.com>
2019-12-14 13:29:28 -0300

    Correção url -  endereço microserviço fornecedor

--- store/src/main/java/com/izabelrodrigues/microservices/store/Constants.java
+++ store/src/main/java/com/izabelrodrigues/microservices/store/Constants.java
@@ -3,6 +3,6 @@ package com.izabelrodrigues.microservices.store;
 public class Constants {
 
 	public static class IntegrationServices {
-		public static final String URL_SERVICE_SUPPLIER = "http://localhost:8081/info";
+		public static final String URL_SERVICE_SUPPLIER = "http://localhost:8081/info/";
 	}
 }
