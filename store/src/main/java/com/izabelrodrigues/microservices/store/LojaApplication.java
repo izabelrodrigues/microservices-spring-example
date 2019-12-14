d7472b82148297ddd0c3fdef8f9483db114c8447
Izabel Rodrigues <izabel.castro.rodrigues@gmail.com>
2019-12-14 15:23:36 -0300

    Configurações eureka server

--- store/src/main/java/com/izabelrodrigues/microservices/store/LojaApplication.java
+++ store/src/main/java/com/izabelrodrigues/microservices/store/LojaApplication.java
@@ -2,9 +2,24 @@ package com.izabelrodrigues.microservices.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class LojaApplication {
+
+	/**
+	 * Obtem uma instancia de rest template capaz de resolver o nome no eureka
+	 * server
+	 * 
+	 * @return
+	 */
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
--- store/src/main/java/com/izabelrodrigues/microservices/store/service/CompraService.java
+++ store/src/main/java/com/izabelrodrigues/microservices/store/service/CompraService.java
@@ -1,5 +1,6 @@
 package com.izabelrodrigues.microservices.store.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -12,10 +13,13 @@ import com.izabelrodrigues.microservices.store.dto.InfoFornecedorDTO;
 @Service
 public class CompraService {
 
+	@Autowired
+	private RestTemplate client;
+
 	public void processar(CompraDTO compra) {
 
 		String estado = compra.getEndereco().getEstado();
-		RestTemplate client = new RestTemplate();
+
 		ResponseEntity<InfoFornecedorDTO> exchange = client.exchange(
 				Constants.IntegrationServices.URL_SERVICE_SUPPLIER + estado, HttpMethod.GET, null,
 				InfoFornecedorDTO.class);
